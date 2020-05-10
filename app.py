c3926552eaea28412a354934a3b41b3b16adbc08
veegaaa <ageev_alex@mail.ru>
2020-05-10 13:57:02 +0300

    * Add content_data.py * Adapt template to content_data.py (add Jinja)

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask import render_template
+from data import content_data
 
 app = Flask(__name__)
 
 @app.route("/")
 def template_index():
-    return render_template("index.html")
+    return render_template("index.html",
+                           title=content_data.title,
+                           departures=content_data.departures,
+                           subtitle=content_data.subtitle,
+                           description=content_data.description,
+                           tours=content_data.tours,
+                           )
 
 @app.route("/departure/<int:num_depart>")
 def template_departure(num_depart):
